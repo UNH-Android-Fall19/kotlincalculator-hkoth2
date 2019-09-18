463b205273331136d4d168fad944fddadafaa89a
hemakothari12 <hkoth2@unh.newhaven.edu>
2019-09-18 18:32:42 -0400

    Cal App changes

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,104 +1,82 @@
 package com.example.calculator
 
+import androidx.appcompat.app.AppCompatActivity
 import net.objecthunter.exp4j.ExpressionBuilder
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    // TextView used to display the input and output
-    lateinit var txtInput: TextView
-
-    // Represent whether the lastly pressed key is numeric or not
-    var lastNumeric: Boolean = false
-
-    // Represent that current state is in error or not
-    var stateError: Boolean = false
-
-    // If true, do not allow to add another DOT
-    var lastDot: Boolean = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        txtInput = findViewById(R.id.txtInput)
-    }
 
-    /**
-     * Append the Button.text to the TextView
-     */
-    fun onDigit(view: View) {
-        if (stateError) {
-            // If current state is Error, replace the error message
-            txtInput.text = (view as Button).text
-            stateError = false
-        } else {
-            // If not, already there is a valid expression so append to it
-            txtInput.append((view as Button).text)
+        //Numbers
+        tvOne.setOnClickListener { appendOnExpresstion("1", true) }
+        tvTwo.setOnClickListener { appendOnExpresstion("2", true) }
+        tvThree.setOnClickListener { appendOnExpresstion("3", true) }
+        tvFour.setOnClickListener { appendOnExpresstion("4", true) }
+        tvFive.setOnClickListener { appendOnExpresstion("5", true) }
+        tvSix.setOnClickListener { appendOnExpresstion("6", true) }
+        tvSeven.setOnClickListener { appendOnExpresstion("7", true) }
+        tvEight.setOnClickListener { appendOnExpresstion("8", true) }
+        tvNine.setOnClickListener { appendOnExpresstion("9", true) }
+        tvZero.setOnClickListener { appendOnExpresstion("0", true) }
+        tvDot.setOnClickListener { appendOnExpresstion(".", true) }
+
+        //Operators
+        tvPlus.setOnClickListener { appendOnExpresstion("+", false) }
+        tvMinus.setOnClickListener { appendOnExpresstion("-", false) }
+        tvMul.setOnClickListener { appendOnExpresstion("*", false) }
+        tvDivide.setOnClickListener { appendOnExpresstion("/", false) }
+        tvOpen.setOnClickListener { appendOnExpresstion("(", false) }
+        tvClose.setOnClickListener { appendOnExpresstion(")", false) }
+
+        tvClear.setOnClickListener {
+            tvExpression.text = ""
+            tvResult.text = ""
         }
-        // Set the flag
-        lastNumeric = true
-    }
 
-    /**
-     * Append . to the TextView
-     */
-    fun onDecimalPoint(view: View) {
-        if (lastNumeric && !stateError && !lastDot) {
-            txtInput.append(".")
-            lastNumeric = false
-            lastDot = true
-        }
-    }
-
-    /**
-     * Append +,-,*,/ operators to the TextView
-     */
-    fun onOperator(view: View) {
-        if (lastNumeric && !stateError) {
-            txtInput.append((view as Button).text)
-            lastNumeric = false
-            lastDot = false    // Reset the DOT flag
-        }
-    }
-
-
-    /**
-     * Clear the TextView
-     */
-    fun onClear(view: View) {
-        this.txtInput.text = ""
-        lastNumeric = false
-        stateError = false
-        lastDot = false
-    }
-
-    /**
-     * Calculate the output using Exp4j
-     */
-    fun onEqual(view: View) {
-        // If the current state is error, nothing to do.
-        // If the last input is a number only, solution can be found.
-        if (lastNumeric && !stateError) {
-            // Read the expression
-            val txt = txtInput.text.toString()
-            // Create an Expression (A class from exp4j library)
-            val expression = ExpressionBuilder(txt).build()
-            try {
-                // Calculate the result and display
-                val result = expression.evaluate()
-                txtInput.text = result.toString()
-                lastDot = true // Result contains a dot
-            } catch (ex: ArithmeticException) {
-                // Display an error message
-                txtInput.text = "Error"
-                stateError = true
-                lastNumeric = false
+        tvBack.setOnClickListener {
+            val string = tvExpression.text.toString()
+            if(string.isNotEmpty()){
+                tvExpression.text = string.substring(0,string.length-1)
             }
+            tvResult.text = ""
+        }
+
+        tvEquals.setOnClickListener {
+            try {
+
+                val expression = ExpressionBuilder(tvExpression.text.toString()).build()
+                val result = expression.evaluate()
+                val longResult = result.toLong()
+                if(result == longResult.toDouble())
+                    tvResult.text = longResult.toString()
+                else
+                    tvResult.text = result.toString()
+
+            }catch (e:Exception){
+//                Log.d( "Exception"," message : " + e.message )
+            }
+        }
+
+    }
+
+    fun appendOnExpresstion(string: String, canClear: Boolean) {
+
+        if(tvResult.text.isNotEmpty()){
+            tvExpression.text = ""
+        }
+
+        if (canClear) {
+            tvResult.text = ""
+            tvExpression.append(string)
+        } else {
+            tvExpression.append(tvResult.text)
+            tvExpression.append(string)
+            tvResult.text = ""
         }
     }
 }
